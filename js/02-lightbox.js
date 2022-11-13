370ee7ef37c6c4a1d503b1037d6861b89c1c7e83
heywhatsup88 <heywhatsup@ukr.net>
2022-11-13 03:20:06 +0300

    Create 02-lightbox.js

---
+++ js/02-lightbox.js
@@ -0,0 +1,65 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// Делаем разметку
+
+const galleryMarkup = createGalleryCardMarkup(galleryItems);
+
+function createGalleryCardMarkup(galleryItem) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>`;
+    })
+    .join("");
+}
+
+// Рендерим в разметку
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+// Делегируем событие клика на div галерею
+
+galleryEl.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  // simpleLightbox
+
+  `<div class="gallery">
+    <a href="${event.target.getAttribute(
+      "href"
+    )}"><img src="${event.target.getAttribute(
+    "src"
+  )}" alt="${event.target.getAttribute("alt")}" /></a>
+    <a href="${event.target.getAttribute(
+      "href"
+    )}"><img src="images/thumbs/thumb2.jpg" alt="${event.target.getAttribute(
+    "alt"
+  )}"/></a>
+</div>
+    `;
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    showCounter: false,
+  });
+  gallery.on("show.simplelightbox");
+}
+
+// console.log(galleryMarkup);
