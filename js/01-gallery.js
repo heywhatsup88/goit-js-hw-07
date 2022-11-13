10208668bf71b14769eeb82f7fd55be097116137
heywhatsup88 <heywhatsup@ukr.net>
2022-11-13 03:13:54 +0300

    .

---
+++ js/01-gallery.js
@@ -0,0 +1,58 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// Делаем разметку
+
+const galleryMarkup = createGalleryCardMarkup(galleryItems);
+
+function createGalleryCardMarkup(galleryItem) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${original}"
+                        data-source="${preview}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>`;
+    })
+    .join("");
+}
+
+// Рендерим в разметку
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+// Делегируем событие клика на div галерею
+
+galleryEl.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  // basicLightbox
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.getAttribute("src")}">
+`);
+
+  instance.show();
+
+  if (instance.show()) {
+    window.addEventListener("keydown", onKeyDown);
+
+    function onKeyDown(event) {
+      if (event.key === "Escape") {
+        instance.close();
+      }
+    }
+  }
+}
